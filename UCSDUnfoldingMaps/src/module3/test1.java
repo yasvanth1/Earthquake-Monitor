b51f4456a5b9c1b10fdfba2835e9cb39a98e50a8
unknown <yasvanth1@hotmail.co.uk>
2020-07-27 20:13:26 +0100

    PApplet Image add/resize

--- UCSDUnfoldingMaps/src/module3/test1.java
+++ UCSDUnfoldingMaps/src/module3/test1.java
@@ -4,16 +4,19 @@ import processing.core.*;
 
 public class test1 extends PApplet {
 
-	private String URL = "";
+	private String URL = "https://upload.wikimedia.org/wikipedia/commons/a/af/Miami_Beach_-_Sand_Dune_Flora_-_Coconut_Palms.jpg";
 	private PImage backgroundImg;
 	
 	
 	public void setup() {
 		size(200,200);
-		backgroundImg = loadImage(URL,"Jjpg");
+		backgroundImg = loadImage(URL,"jpg");
 	}
 	
 	public void draw () {
+		
+		backgroundImg.resize(200, 200);
+		image(backgroundImg, 0,0);
 		
 	}
 	
