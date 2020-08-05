23a8205d02a22008f89e59056e08e51339dc3650
unknown <yasvanth1@hotmail.co.uk>
2020-08-05 21:48:46 +0100

    Creating a gui elements which react to time. colour change due to time change will be useful when making the time stamps on a map.

--- UCSDUnfoldingMaps/src/module3/test1.java
+++ UCSDUnfoldingMaps/src/module3/test1.java
@@ -17,8 +17,29 @@ public class test1 extends PApplet {
 		
 		backgroundImg.resize(0, 200); //0 so after giving height Java will automatically adjust the width to fit the image (dynamic)
 		image(backgroundImg, 0,0);
-		ellipse(width/4,height/5,width/5,height/5);
+		int [] color = SunColorSec(second());
+		fill(color [01],color [1], color [2]);				// sunny yellow colour
+		ellipse(width/7,height/5,width/7,height/7);
 		
 	}
+	
+	public int[] SunColorSec (float seconds) {
+
+		int[] rgb = new int [3]; // made a new array which will hold 3 values rgb
+		
+		float diffFrom30 = Math.abs(30 - seconds);
+		
+		float ratio = diffFrom30/30;
+		rgb[0] = (int) (255*ratio);
+		rgb[1]=  (int)(255*ratio); 
+		rgb[2] = 0;
+		
+		
+		return rgb;
+		
+		
+		
+	}
+	
 	
 }
